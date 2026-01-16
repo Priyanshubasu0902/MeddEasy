3764217f363b118ea73acb0f37579377680b9bb5
priyanshu.b0902@gmail.com <priyanshu.b0902@gmail.com>
2026-01-16 15:35:53 +0530

    modification

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [emailType, setEmailType] = useState(true);
   const [loading, setLoading] = useState(false);
+  
   const [numberIsValid, setNumberIsValid] = useState("");
 
   const { backendUrl, setUserToken } = useContext(AppContext);
